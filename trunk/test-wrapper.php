563ad6b795a7333e43e6c9e7db0b64a4194f4c21
vmvarela <vmvarela@f4163266-a019-0410-9c8b-7fcd5782fe3a>
2013-05-17 12:39:45 +0000

    ok

--- trunk/test-wrapper.php
+++ trunk/test-wrapper.php
@@ -2,40 +2,63 @@
 
 include './smbwebclient.php';
 
+
+$smbclientpath = 'smbclient://sprfiles/TI';
+$filepath = './TI';
+
 smbclient_stream_wrapper::SetAuth('isotroldom\vmvarela', 'queypo');
-$path = 'smbclient://sprfiles/TI';
 
-mkdir($path.'/test-vmvarela');
-mkdir($path.'/test-vmvarela/dir1');
-mkdir($path.'/test-vmvarela/dir2');
-rmdir($path.'/test-vmvarela/dir2');
 
-$filename = $path.'/test-vmvarela/dir1/fil1.txt';
-$errfile = $filename.'_';
+
+
+$path = $smbclientpath;
+$dir  = $path.'/test-vmvarela';
+$errdir  = $path.'/test-vmvarelaXXX';
+$filename = $dir.'/dir1/nombreráro.txt';
+$errfile = $filename.'XXX';
+
+/*
+echo "\n".$smbclientpath.":";
+print_r(stat($smbclientpath.'/test-vmvarela'));
+echo "\n".$filepath.":";
+print_r(stat($filepath.'/test-vmvarela'));
+exit;
+*/
+
+
+
+
+if (! @mkdir($dir)) echo "ya existe test-vmvarela\n";
+if (! @mkdir($dir.'/dir1')) echo "ya existe dir1\n";
+if (! @mkdir($dir.'/dir2')) echo "ya existe dir2\n";
+if (! rmdir($dir.'/dir2')) echo "no existe dir2\n";
+
+
+
+
+// smbclient_stream_wrapper::$debugLevel = 3;
+
+
+
+# FALLO: ARCHIVO CON COMILLAS DOBLES "
 
 $fd = fopen($filename, 'w');
 fputs($fd, 'ok');
 fclose($fd);
 
+$fd = fopen($filename, 'r');
+$text = fread($fd, 100);
+echo $text."\n";
+fclose($fd);
 
-# is_file($filename) OR print("error is_file $filename\n");
-# file_exists($filename) OR print("error file_exists $filename\n");
-
-# is_file($errfile) OR print("error is_file $errfile\n");
-# file_exists($errfile) OR print("error file_exists $errfile\n");
-
-is_dir($path) OR print("share $path not is_dir\n");
-file_exists($path) OR print("share $path not file_exists\n");
-
-# echo "stat $filename:\n";
-# print_r(stat($filename));
-
-# $fd = fopen($filename, 'r');
-# $text = fread($fd, 100);
-# echo $text."\n";
-# fclose($fd);
-
-
-
+foreach (array($path, $dir, $errdir, $filename, $errfile) as $x) {
+  print ($x . (file_exists($x) ? " " : " NOT ") . "file_exists\n");
+  print ($x . (is_dir($x) ? " " : " NOT ") . "is_dir\n");
+  print ($x . (is_executable($x) ? " " : " NOT ") . "is_executable\n");
+  print ($x . (is_file($x) ? " " : " NOT ") . "is_file\n");
+  print ($x . (is_link($x) ? " " : " NOT ") . "is_link\n");
+  print ($x . (is_readable($x) ? " " : " NOT ") . "is_readable\n");
+  print ($x . (is_writable($x) ? " " : " NOT ") . "is_writable\n");
+}
 
 ?>
